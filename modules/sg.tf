df8b5b6bdff8edfb24142b307628a51d1b1ec3bd
arun291091 <30621005+arun291091@users.noreply.github.com>
2022-08-08 15:59:49 +0530

    Update sg.tf

--- modules/sg.tf
+++ modules/sg.tf
@@ -9,6 +9,31 @@ resource "aws_security_group" "secgrp" {
 
   ingress {
     description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.base.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  
+}
+
+
+resource "aws_security_group" "lb_sg" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.base.id
+
+  ingress {
+    description      = "TLS from VPC"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
