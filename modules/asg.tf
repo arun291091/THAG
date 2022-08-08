cf57f455fe899f477b648ee3a6c0f268d49d0015
arun291091 <30621005+arun291091@users.noreply.github.com>
2022-08-08 16:03:41 +0530

    Update asg.tf

--- modules/asg.tf
+++ modules/asg.tf
@@ -4,7 +4,7 @@ resource "aws_launch_template" "thag" {
   instance_type = "t2.micro"
 }
 
-resource "aws_autoscaling_group" "bar" {
+resource "aws_autoscaling_group" "thag" {
   availability_zones = ["us-west-1a", "us-west-1b", "us-west-1c"]
   desired_capacity   = 3
   max_size           = 5
@@ -14,4 +14,11 @@ resource "aws_autoscaling_group" "bar" {
     id      = aws_launch_template.thag.id
     version = aws_launch_template.thag.latest_version
   }
+}
+
+
+  
+  resource "aws_autoscaling_attachment" "thag_asg_Attachment" {
+  autoscaling_group_name = aws_autoscaling_group.thag.id
+  lb_target_group_arn    = aws_lb_target_group.thag.arn
 }
