738f9a9f62a140181cce23f39994fbbfbc57c6ed
arun291091 <30621005+arun291091@users.noreply.github.com>
2022-08-08 16:14:42 +0530

    Update asg.tf

--- modules/asg.tf
+++ modules/asg.tf
@@ -2,6 +2,7 @@ resource "aws_launch_template" "thag" {
   name_prefix   = "thag"
   image_id      = data.aws_ami.amazon-2.id
   instance_type = "t2.micro"
+  user_data = filebase64("${path.module}/init.sh")
 }
 
 resource "aws_autoscaling_group" "thag" {
