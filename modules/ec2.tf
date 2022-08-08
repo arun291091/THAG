c93f90b3e131a2bf89f0ba6bcd835efc290caeae
arun291091 <30621005+arun291091@users.noreply.github.com>
2022-08-08 15:34:24 +0530

    Create ec2.tf

---
+++ modules/ec2.tf
@@ -0,0 +1,38 @@
+
+
+
+############ Ec2 #####################
+
+  resource "aws_key_pair" "key" {
+  key_name   = "demo1"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+
+
+data "aws_ami" "amazon-2" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+  owners = ["amazon"]
+}
+
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon-2.id
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.key.key_name
+  security_groups = [aws_security_group.secgrp.id]
+  tags = {
+    Name = "zeta-1"
+  }
+  
+}
+
+output "ec2_id" {
+  value = aws_instance.web.id
+
+}
