a86d3d5454ded818926928f893c5941394ec5985
arun291091 <30621005+arun291091@users.noreply.github.com>
2022-08-08 15:34:43 +0530

    Create sg.tf

---
+++ modules/sg.tf
@@ -0,0 +1,27 @@
+### Security Group #################
+
+
+
+resource "aws_security_group" "secgrp" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.base.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.base.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  
+}
